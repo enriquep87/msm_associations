60f1e982c0350259abdd10e14b12650572b6e8e5
Enrique P <ername@users.noreply.github.com>
2016-11-14 18:58:02 -0600

    Validations complete

--- app/models/director.rb
+++ app/models/director.rb
@@ -1,4 +1,4 @@
 class Director < ActiveRecord::Base
   validates(:name, {:presence => true, :uniqueness => true })
-  alidates(:name, uniqueness: {scope: :dob, message:"Director already exists"})
+  validates(:name, uniqueness: {scope: :dob, message:"Director already exists"})
 end
